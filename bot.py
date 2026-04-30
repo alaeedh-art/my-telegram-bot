f419fe9b93b6a2fb3b85775b9d614d4868063bc7
alaeedh-art <alaeedh@gmail.com>
2026-04-30 06:25:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 TOKEN = "حط التوكن هنا"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("هلا 👋 البوت شغال 24/7")
+    await update.message.reply_text("هلا بيك 👋")
 
 app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
